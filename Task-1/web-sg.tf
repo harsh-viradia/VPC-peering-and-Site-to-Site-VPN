7f0d19285402f4d96be9204a9ffa609506211e6d
Harsh Viradia <harsh.viradia@intuitive.cloud>
2023-04-04 21:29:10 +0530

    Update web-sg.tf

--- Task-1/web-sg.tf
+++ Task-1/web-sg.tf
@@ -6,7 +6,7 @@ resource "aws_security_group" "harsh-viradia-web-sg" {
     from_port   = 80
     to_port     = 80
     protocol    = "tcp"
-    cidr_blocks = ["10.0.0.0/18"]
+    cidr_blocks = ["10.0.64.0/18"]
   }
 
   egress {
