6a486e017dde8abee1a6b081e555a854f06f7f7d
Harsh Viradia <harsh.viradia@intuitive.cloud>
2023-04-07 19:48:45 +0530

    Delete Task-1 directory

--- Task-1/ami.tf
+++
@@ -1,8 +0,0 @@
-data "aws_ami" "web_ami" {
-  owners      = ["amazon"]
-  most_recent = true
-  filter {
-    name   = "name"
-    values = ["amzn2-ami-kernel-5.10-hvm-*"]
-  }
-}
--- Task-1/peering.tf
+++
@@ -1,5 +0,0 @@
-resource "aws_vpc_peering_connection" "harsh-viradia-vpc-peering" {
-  vpc_id = aws_vpc.harsh-viradia-security-vpc.id
-  peer_vpc_id = aws_vpc.harsh-viradia-web-vpc.id
-  auto_accept = true
-}
